ed121419f3d3819ba72cdb3abe90986cc15a60b0
Adem OZTURK <admoztrk86@gmail.com>
2022-12-09 00:07:42 +0100

    next is done

--- src/test/java/HelloJava.java
+++ src/test/java/HelloJava.java
@@ -2,5 +2,7 @@ public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello test");
         System.out.println("Test will soon start");
+        System.out.println("DO something else");
+        System.out.println("Test will soon start");
     }
 }
